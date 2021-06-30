b76ce0fd3f3cfd21e7048da371e2a87a96b3eb79
John Nolan <john.nolan@digital.justice.gov.uk>
2021-06-30 11:42:09 +0100

    Remove CNAME, remove JS

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ const markdownItGds = require("./markdown-it-gds");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ 'node_modules/@ministryofjustice/frontend/assets': 'assets' })
-    eleventyConfig.addPassthroughCopy({ 'node_modules/govuk-frontend/govuk/*.js': 'assets/javascript' })
     eleventyConfig.addPassthroughCopy({ 'node_modules/govuk-frontend/govuk/assets': 'assets' })
     eleventyConfig.addPassthroughCopy({ 'src/_includes/css': 'assets' })
     eleventyConfig.addPassthroughCopy({ 'src/_includes/assets': 'assets' })
@@ -11,32 +10,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(inclusiveLangPlugin);
 
     eleventyConfig.setLibrary("md", markdownItGds());
-
-    eleventyConfig.addFilter(
-        "addActiveAttribute",
-        function (config, filePathStem) {
-            if (config.items) {
-                return {
-                    ...config,
-                    items: config.items.map((item) => ({
-                        ...item,
-                        active: filePathStem.indexOf(item.href) > -1,
-                    })),
-                };
-            } else if (config.sections) {
-                return {
-                    ...config,
-                    sections: config.sections.map((section) => ({
-                        ...section,
-                        items: section.items.map((item) => ({
-                            ...item,
-                            active: filePathStem.indexOf(item.href) > -1,
-                        })),
-                    })),
-                };
-            }
-        }
-    );
 
     return {
         dir: { input: 'src', output: '_site', data: '_data' },

bd3cc29b1b80a185998c261a5fa55615baf890a7
John Nolan <john.nolan@digital.justice.gov.uk>
2022-03-08 08:40:03 +0000

    Change contents from span to div to remove whitespace (#42)

--- .eleventy.js
+++ .eleventy.js
@@ -12,7 +12,7 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPlugin(inclusiveLangPlugin);
     eleventyConfig.addPlugin(pluginTOC, {
-        wrapper: 'span',
+        wrapper: 'div',
         wrapperClass: 'govuk-list govuk-list--bullet',
         ul: true
     });

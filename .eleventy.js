94256bacda85ed9f7ba0c258ed6fd1c9bd2ff23b
John Nolan <john.nolan@digital.justice.gov.uk>
2022-12-29 13:15:20 +0000

    Linting fixes

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,17 @@
-const inclusiveLangPlugin = require("@11ty/eleventy-plugin-inclusive-language");
-const markdownItGds = require("./markdown-it-gds");
+const inclusiveLangPlugin = require('@11ty/eleventy-plugin-inclusive-language');
+const markdownItGds = require('./markdown-it-gds');
 const dateFilter = require('./src/filters/date-filter.js');
 const date24HourFilter = require('./src/filters/date24Hours-filter.js');
 //const pluginTOC = require('eleventy-plugin-toc');
 const pluginTOC = require('./extensions/eleventy-plugin-toc/.eleventy.js');
-const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginRss = require('@11ty/eleventy-plugin-rss');
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy({ 'node_modules/govuk-frontend/govuk/assets': 'assets' })
-    eleventyConfig.addPassthroughCopy({ 'src/_includes/css': 'assets' })
-    eleventyConfig.addPassthroughCopy({ 'src/_includes/assets': 'assets' })
-    eleventyConfig.addPassthroughCopy({ 'src/_includes/scripts': 'assets' })
-    eleventyConfig.addPassthroughCopy({ 'src/_includes/budget.json': 'budget.json' })
+    eleventyConfig.addPassthroughCopy({ 'node_modules/govuk-frontend/govuk/assets': 'assets' });
+    eleventyConfig.addPassthroughCopy({ 'src/_includes/css': 'assets' });
+    eleventyConfig.addPassthroughCopy({ 'src/_includes/assets': 'assets' });
+    eleventyConfig.addPassthroughCopy({ 'src/_includes/scripts': 'assets' });
+    eleventyConfig.addPassthroughCopy({ 'src/_includes/budget.json': 'budget.json' });
 
     eleventyConfig.addPlugin(inclusiveLangPlugin);
     eleventyConfig.addPlugin(pluginRss);
@@ -23,17 +23,17 @@ module.exports = function (eleventyConfig) {
     });
 
     eleventyConfig.addFilter('log', value => {
-        console.log(value)
+        console.log(value);
     })
 
-    eleventyConfig.addFilter("limit", function (array, limit) {
+    eleventyConfig.addFilter('limit', function (array, limit) {
         return array.slice(0, limit);
     });
 
     eleventyConfig.addFilter('dateFilter', dateFilter);
     eleventyConfig.addFilter('date24HourFilter', date24HourFilter);
 
-    eleventyConfig.setLibrary("md", markdownItGds());
+    eleventyConfig.setLibrary('md', markdownItGds());
 
     return {
         dir: { input: 'src', output: '_site', data: '_data' },
